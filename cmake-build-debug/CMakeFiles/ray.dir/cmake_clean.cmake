09031e072437702da4caca5583a5422f80dfffee
halinkim <halin1028@naver.com>
2023-09-30 21:42:42 +0900

    cmake

---
+++ cmake-build-debug/CMakeFiles/ray.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ray.dir/main.cpp.obj"
+  "CMakeFiles/ray.dir/main.cpp.obj.d"
+  "libray.dll.a"
+  "ray.exe"
+  "ray.exe.manifest"
+  "ray.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ray.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/halin/CLionProjects/ray
+# Install script for directory: //wsl.localhost/Ubuntu/home/halin/halinkim/ray
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -34,7 +34,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/Program Files/JetBrains/CLion 2023.2/bin/mingw/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "C:/Program Files/JetBrains/CLion 2022.2.3/bin/mingw/bin/objdump.exe")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/halin/CLionProjects/ray/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "//wsl.localhost/Ubuntu/home/halin/halinkim/ray/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
