69544d1f1010a639d1ddc6b7da10e19df7267d22
halinkim <halin1028@gmail.com>
2023-10-17 20:00:52 +0900

    anti-aliasing

--- main.cpp
+++ main.cpp
@@ -4,14 +4,34 @@
 
 #include <iostream>
 #include <fstream>
+#include <vector>
 #include "vec.h"
 #include "ray.h"
 
-vec ray_color(ray r) {
-    vec unit_direction = unit_vector(r.direction());
-    double a = 0.5*(unit_direction.z() + 1.0);
+std::vector<vec> sphere_origins;
+std::vector<double> sphere_radiuss;
 
-    return (1.0-a)*vec(1.0, 1.0, 1.0) + a*vec(0.5, 0.7, 1.0);
+vec ray_color(ray r) {
+    double max_t = -1;
+
+    for (int i = 0; i < sphere_radiuss.size(); ++i) {
+        vec sphere_origin = sphere_origins[i];
+        double sphere_radius = sphere_radiuss[i];
+        vec a = sphere_origin - r.ori;
+        vec b = r.dir;
+        double t = dot(a, b) / b.lensq();
+        double dist = (a - t * b).len();
+        if (dist <= sphere_radius) {
+
+            return {1.0, 0.0, 0.0};
+        }
+    }
+
+
+    vec unit_direction = unit_vector(r.direction());
+    double c = 0.5*(unit_direction.z() + 1.0);
+
+    return (1.0-c)*vec(1.0, 1.0, 1.0) + c*vec(0.5, 0.7, 1.0);
 }
 
 int main() {
@@ -53,8 +73,19 @@ int main() {
     vec pix_du = unit_vector(cross(pix_dv, focal_length)) * viewport_width / image_width;
     vec pix0 = focal_length - pix_du * image_width / 2 - pix_dv * image_height / 2;
 
+    int anti_aliasing_num = 10;
+
     std::cout << "pix_du:\t" << pix_du[0] << '\t' << pix_du[1] << '\t' << pix_du[2] << '\n';
     std::cout << "pix_dv:\t" << pix_dv[0] << '\t' << pix_dv[1] << '\t' << pix_dv[2] << '\n';
+
+
+    sphere_origins.push_back(vec(1, 0.3, 0));
+    sphere_origins.push_back(vec(1, 0.5, 0));
+    sphere_origins.push_back(vec(1.5, 2, 0));
+    sphere_radiuss.push_back(0.2);
+    sphere_radiuss.push_back(0.1);
+    sphere_radiuss.push_back(0.3);
+
 
     /*
      * Image Processing.
@@ -62,10 +93,14 @@ int main() {
     for (int j = 0; j < image_height; ++j) {
         std::cout << "\rProcessing.. " << j + 1 << " of " << image_height << std::flush;
         for (int i = 0; i < image_width; ++i) {
-            vec pix = pix0 + ((i + 0.5) * pix_du) + ((j + 0.5) * pix_dv);
-
-            vec rgb = ray_color(ray(camera_pos, pix));
-
+            vec rgb;
+            for (int _j = 0; _j < anti_aliasing_num; ++_j) {
+                for (int _i = 0; _i < anti_aliasing_num; ++_i) {
+                    vec pix = pix0 + ((i + (0.5 + _i) / anti_aliasing_num) * pix_du) + ((j + (0.5 + _j) / anti_aliasing_num) * pix_dv);
+                    rgb += ray_color(ray(camera_pos, pix));
+                }
+            }
+            rgb /= anti_aliasing_num * anti_aliasing_num;
             int ir = static_cast<int>(255.999 * rgb[0]);
             int ig = static_cast<int>(255.999 * rgb[1]);
             int ib = static_cast<int>(255.999 * rgb[2]);
